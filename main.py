22bcb56ab2dea26c3ca77dbf21ad47d3d003e781
Denis Kuzminykh <reaper.of.midnight@gmail.com>
2016-11-19 15:03:54 +0300

    Initial commit

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from cnn_model_with_output_conv import CNNModel
 
 model = CNNModel()
 
-train_model(model, "cnn_model_with_output_conv", 20)
+train_model(model, "cnn_model_with_output_conv", 20000)
 test_model(model, "cnn_model_with_output_conv")
